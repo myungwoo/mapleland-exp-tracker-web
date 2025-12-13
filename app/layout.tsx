13fc4f557411389bfd436f6468e6ba9b0b8125af
Myungwoo Chun <mc.tamaki@gmail.com>
2025-12-12 23:23:44 -0800

    페이지 제목 설명 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-	title: "EXP Tracker",
-	description: "Measure EXP gain over time from a selected game window"
+	title: "메이플랜드 경험치 측정기",
+	description: "게임 화면에서 레벨과 경험치를 인식하여 경험치를 측정하는 웹앱입니다."
 };
 
 export default function RootLayout(props: { children: React.ReactNode }) {
